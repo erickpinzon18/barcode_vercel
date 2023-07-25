4b6db5013602b35910f04af67566cbb2541bbcf3
Erick Pinzon <erickph.ti21@utsjr.edu.mx>
2023-07-24 19:37:15 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ import express from "express";
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-    res.render("index");
+    res.render("index.ejs");
 });
 
 export default routes;
