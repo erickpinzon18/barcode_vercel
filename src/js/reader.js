44e5cf06989e233b241281d0982bb25944ead8e0
Erick Pinzon <65203105+erickpinzon18@users.noreply.github.com>
2024-03-27 21:08:27 -0600

    use DataTables

--- src/js/reader.js
+++ src/js/reader.js
@@ -3,90 +3,90 @@ window.onload = $("#label_detenido").hide();
 
 $("#nav_reader").addClass("active");
 
-document.addEventListener("DOMContentLoaded", () => {
-	Quagga.init(
-		{
-			inputStream: {
-				constraints: {
-					width: 1920,
-					height: 1080,
-				},
-				name: "Live",
-				type: "LiveStream",
-				target: document.querySelector("#contenedor"), // Pasar el elemento del DOM
-			},
-			decoder: {
-				readers: ["ean_reader"],
-			},
-		},
-		function (err) {
-			if (err) {
-				$("#modal_error").html("Error al iniciar la cámara");
-				$("#modal_error_body").html(
-					"<p>Ha ocurrido un error al iniciar la cámara</p><br><p>Por favor, asegurese de dar permisos para utilizar la cámara como scanner</p>"
-				);
-				alert('No se ha proporcionado acceso a la cámara, por lo cual no se podrá utilizar como scanner')
-				//$("#modalError").modal("show");
-				$("#label_detenido").show();
-				$("#label_leyendo").hide();
-				console.log(err);
-				return;
-			}
-			console.log("Iniciado correctamente");
-			Quagga.start();
-		}
-	);
+// document.addEventListener("DOMContentLoaded", () => {
+	// Quagga.init(
+	// 	{
+	// 		inputStream: {
+	// 			constraints: {
+	// 				width: 1920,
+	// 				height: 1080,
+	// 			},
+	// 			name: "Live",
+	// 			type: "LiveStream",
+	// 			target: document.querySelector("#contenedor"), // Pasar el elemento del DOM
+	// 		},
+	// 		decoder: {
+	// 			readers: ["ean_reader"],
+	// 		},
+	// 	},
+	// 	function (err) {
+	// 		if (err) {
+	// 			$("#modal_error").html("Error al iniciar la cámara");
+	// 			$("#modal_error_body").html(
+	// 				"<p>Ha ocurrido un error al iniciar la cámara</p><br><p>Por favor, asegurese de dar permisos para utilizar la cámara como scanner</p>"
+	// 			);
+	// 			alert('No se ha proporcionado acceso a la cámara, por lo cual no se podrá utilizar como scanner')
+	// 			//$("#modalError").modal("show");
+	// 			$("#label_detenido").show();
+	// 			$("#label_leyendo").hide();
+	// 			console.log(err);
+	// 			return;
+	// 		}
+	// 		console.log("Iniciado correctamente");
+	// 		Quagga.start();
+	// 	}
+	// );
 
-	Quagga.onDetected((data) => {
-		if (!scanned) barcodeRead(data.codeResult.code);
-	});
+	// Quagga.onDetected((data) => {
+	// 	if (!scanned) barcodeRead(data.codeResult.code);
+	// });
 
-	Quagga.onProcessed(function (result) {
-		var drawingCtx = Quagga.canvas.ctx.overlay,
-			drawingCanvas = Quagga.canvas.dom.overlay;
+	// Quagga.onProcessed(function (result) {
+	// 	var drawingCtx = Quagga.canvas.ctx.overlay,
+	// 		drawingCanvas = Quagga.canvas.dom.overlay;
 
-		if (result) {
-			if (result.boxes) {
-				drawingCtx.clearRect(
-					0,
-					0,
-					parseInt(drawingCanvas.getAttribute("width")),
-					parseInt(drawingCanvas.getAttribute("height"))
-				);
-				result.boxes
-					.filter(function (box) {
-						return box !== result.box;
-					})
-					.forEach(function (box) {
-						Quagga.ImageDebug.drawPath(
-							box,
-							{ x: 0, y: 1 },
-							drawingCtx,
-							{ color: "green", lineWidth: 2 }
-						);
-					});
-			}
+	// 	if (result) {
+	// 		if (result.boxes) {
+	// 			drawingCtx.clearRect(
+	// 				0,
+	// 				0,
+	// 				parseInt(drawingCanvas.getAttribute("width")),
+	// 				parseInt(drawingCanvas.getAttribute("height"))
+	// 			);
+	// 			result.boxes
+	// 				.filter(function (box) {
+	// 					return box !== result.box;
+	// 				})
+	// 				.forEach(function (box) {
+	// 					Quagga.ImageDebug.drawPath(
+	// 						box,
+	// 						{ x: 0, y: 1 },
+	// 						drawingCtx,
+	// 						{ color: "green", lineWidth: 2 }
+	// 					);
+	// 				});
+	// 		}
 
-			if (result.box) {
-				Quagga.ImageDebug.drawPath(
-					result.box,
-					{ x: 0, y: 1 },
-					drawingCtx,
-					{ color: "#00F", lineWidth: 2 }
-				);
-			}
+	// 		if (result.box) {
+	// 			Quagga.ImageDebug.drawPath(
+	// 				result.box,
+	// 				{ x: 0, y: 1 },
+	// 				drawingCtx,
+	// 				{ color: "#00F", lineWidth: 2 }
+	// 			);
+	// 		}
 
-			if (result.codeResult && result.codeResult.code) {
-				Quagga.ImageDebug.drawPath(
-					result.line,
-					{ x: "x", y: "y" },
-					drawingCtx,
-					{ color: "red", lineWidth: 3 }
-				);
-			}
-		}
-	});
-});
+	// 		if (result.codeResult && result.codeResult.code) {
+	// 			Quagga.ImageDebug.drawPath(
+	// 				result.line,
+	// 				{ x: "x", y: "y" },
+	// 				drawingCtx,
+	// 				{ color: "red", lineWidth: 3 }
+	// 			);
+	// 		}
+	// 	}
+	// });
+// });
 
 function barcodeRead(barcode) {
 	var audio = new Audio("/sounds/beep.mp3");
@@ -168,3 +168,6 @@ var trBoldBlue = $("table");
 $(trBoldBlue).on("click", "tr", function (){
 		$(this).toggleClass("bold-blue");
 });
+
+// Init datatable
+let table = new DataTable('#product_tb');
